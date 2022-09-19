5605f1714af7482c235a0aa3eb4902a69ae1660b
MohsinTaj <mohsintaj@github.com>
2022-09-19 12:33:48 +0500

    first commit

--- my-app/src/script.js
+++ my-app/src/script.js
@@ -26,7 +26,7 @@ window.addEventListener('load', () => {
 
 		const task_actions_el = document.createElement('div');
 		task_actions_el.classList.add('actions');
-		
+
 		const task_edit_el = document.createElement('button');
 		task_edit_el.classList.add('edit');
 		task_edit_el.innerText = 'Edit';
